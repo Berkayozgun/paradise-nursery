8a001d2c9cfd268cb3593f60c820bb4bed2d993a
Berkayozgun <berkayozgun001@gmail.com>
2024-10-10 15:19:53 +0300

    Update lazy loading and suspense in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Header from "./components/Header";
 import Spinner from "./components/Spinner";
 
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const Header = lazy(() => import("./components/Header"));
 const ProductsPage = lazy(() => import("./pages/ProductsPage"));
 const CartPage = lazy(() => import("./pages/CartPage"));
 const CheckoutPage = lazy(() => import("./pages/CheckoutPage"));
@@ -14,19 +14,17 @@ const App = () => {
   const location = useLocation();
 
   return (
-    <>
+    <Suspense fallback={<Spinner />}>
       {location.pathname !== "/" && <Header />}
-      <Suspense fallback={<Spinner />}>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/products' element={<ProductsPage />} />
-          <Route path='/cart' element={<CartPage />} />
-          <Route path='/checkout' element={<CheckoutPage />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </Suspense>
-    </>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/products' element={<ProductsPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Suspense>
   );
 };
 
